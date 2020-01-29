703de14ed6d831e8f0abf6ab0f603a10b4d3eae6
Obo Agboghidi <oboagboghidi@Obos-MacBook-Pro-2.local>
2020-01-29 05:19:48 -0500

    build

---
+++ build/precache-manifest.d6118afdf03457c058b7a44c33a55e3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b2715959ac113a13ae11e088b814f48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eacecd7affcfb91e1b26",
+    "url": "/static/css/2.c4d1b98b.chunk.css"
+  },
+  {
+    "revision": "d9ed64af9d5e730673e4",
+    "url": "/static/css/main.e580f558.chunk.css"
+  },
+  {
+    "revision": "eacecd7affcfb91e1b26",
+    "url": "/static/js/2.d16f6311.chunk.js"
+  },
+  {
+    "revision": "d9ed64af9d5e730673e4",
+    "url": "/static/js/main.4da5ddcd.chunk.js"
+  },
+  {
+    "revision": "64db63436e192ceb8b5d",
+    "url": "/static/js/runtime-main.deff7e42.js"
+  }
+]);
